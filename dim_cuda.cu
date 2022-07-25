9712923d62b0d799f510f8c812aa664536d831bd
Adam Klein <adam3207@gmail.com>
2022-07-24 19:23:24 -0700

    Moved memory management to gpu

--- dim_cuda.cu
+++ dim_cuda.cu
@@ -4,77 +4,228 @@
 using namespace gpu;
 
 template <class T>
-Matrix3D<T>::Matrix3D(size_t num_layers, size_t column_h, size_t row_w) {
+Matrix3D<T>::Matrix3D(size_t num_layers, size_t column_h, size_t row_w, bool preprocess) {
     // set dimensions
     num_layers_ = num_layers;
     column_h_ = column_h;
     row_w_ = row_w;
+    size_bytes = sizeof(T) * num_layers_ * column_h_ * row_w_;
 
     // data is always new allocated for 3D
-    data_ = new T[num_layers_ * column_h_ * row_w_];
+    cudaError_t rval = cudaMalloc(&data_, size_bytes);
+    assert(rval == cudaSuccess);
 
+    // only process if needed
+    if (preprocess) {
+        process();
+    }
+    else {
+        list_ = NULL;
+    }
+}
+
+template <class T>
+Matrix3D<T>::~Matrix3D() {
+    // both were always new allocated
+    if (list_ != NULL) delete[] list_;
+    cudaFree(data_);
+}
+
+template <class T>
+void Matrix3D<T>::process() {
     // create an array of 2D matrices to return if indexed
     list_ = new Matrix2D<T>[num_layers];
     for (int i=0; i < num_layers_; i++) {
         // each matrix should reference piece of memory
-        list_[i] = Matrix2D<T>(column_h_, row_w_, &(data_[i*column_h_*row_w_]));
+        list_[i] = Matrix2D<T>(column_h_, row_w_, data_+(i*column_h_*row_w_));
     }
-}
-template <class T>
-Matrix3D<T>::~Matrix3D() {
-    // both were always new allocated
-    delete[] list_;
-    delete[] data_;
 }
 
 template <class T>
-Matrix2D<T>::Matrix2D(size_t column_h, size_t row_w, T* data_loc) {
+void Matrix3D<T>::write(T* buf) {
+    assert(buf != NULL);
+
+    cudaError_t rval = cudaMemcpy(data_, buf, size_bytes, cudaMemcpyHostToDevice);
+    assert(rval == cudaSuccess);
+}
+
+template <class T>
+T* Matrix3D<T>::read(T* buf) {
+    if (buf == NULL) {
+        buf = new T[num_layers_ * column_h_ * row_w_];
+    }
+
+    cudaError_t rval = cudaMemcpy(buf, data_, size_bytes, cudaMemcpyDeviceToHost);
+    assert(rval == cudaSuccess);
+    return buf;
+}
+
+
+template <class T>
+Matrix2D<T>::Matrix2D(size_t column_h, size_t row_w, T* data_loc, bool preprocess=true) {
     // set dimensions
     column_h_ = column_h;
     row_w_ = row_w;
+    size_bytes = sizeof(T) * column_h_ * row_w_;
 
     // Allocate or reference data. Record if new allocated for later deletion.
     if (data_loc == NULL) {
         internal_ = false;
-        data_ = new T[column_h_*row_w_];
+        cudaError_t rval = cudaMalloc(&data_, size_bytes);
+        assert(rval == cudaSuccess);
     }
     else {
         internal_ = true;
         data_ = data_loc;
     }
 
-    // create an array of 2D matrices to return if indexed
-    layer_ = new Vector1D<T>[column_h_];
-    for (int i=0; i < num_layers_; i++) {
-        // each matrix should reference piece of memory
-        layer_[i] = Vector1D<T>(row_w_, &(data_[i*row_w_]));
+    // only process if needed
+    if (preprocess) {
+        process();
+    }
+    else {
+        layer_ = NULL;
     }
 }
+
 template <class T>
 Matrix2D<T>::~Matrix2D() {
     // always new allocated
-    delete[] layer_;
+    if (layer_ != NULL) delete[] layer_;
     // only delete if originally new allocated
-    if (!internal_) delete[] data_;
+    if (!internal_) cudaFree(data_);
 }
 
 template <class T>
-Vector1D<T>::Vector1D(size_t row_w, T* data_loc) {
+void Matrix2D<T>::process() {
+    // create an array of 2D matrices to return if indexed
+    layer_ = new Matrix1D<T>[column_h_];
+    for (int i=0; i < row_w_; i++) {
+        // each matrix should reference piece of memory
+        layer_[i] = Matrix1D<T>(row_w_, data_+(i*row_w_));
+    }
+}
+
+template <class T>
+void Matrix2D<T>::write(T* buf) {
+    assert(buf != NULL);
+
+    cudaError_t rval = cudaMemcpy(data_, buf, size_bytes, cudaMemcpyHostToDevice);
+    assert(rval == cudaSuccess);
+}
+
+template <class T>
+T* Matrix2D<T>::read(T* buf) {
+    if (buf == NULL) {
+        buf = new T[row_w_ * column_h_];
+    }
+
+    cudaError_t rval = cudaMemcpy(buf, data_, size_bytes, cudaMemcpyDeviceToHost);
+    assert(rval == cudaSuccess);
+    return buf;
+}
+
+
+template <class T>
+Matrix1D<T>::Matrix1D(size_t row_w, T* data_loc, bool preprocess=true) {
     // set dimensions
     row_w_ = row_w;
+    size_bytes = sizeof(T) * row_w_;
 
     // Allocate or reference data. Record if new allocated for later deletion.
     if (data_loc == NULL) {
         internal_ = false;
-        data_ = new T[row_w_];
+        cudaError_t rval = cudaMalloc(&data_, size_bytes);
+        assert(rval == cudaSuccess);
+    }
+    else {
+        internal_ = true;
+        data_ = data_loc;
+    }
+
+    // only process if needed
+    if (preprocess) {
+        process();
+    }
+    else {
+        ptrs_ = NULL;
+    }
+}
+
+template <class T>
+Matrix1D<T>::~Matrix1D() {
+    if (ptrs_ != NULL) delete[] ptrs_;
+    // only delete if originally new allocated
+    if (!internal_) cudaFree(data_);
+}
+
+template <class T>
+void Matrix1D<T>::process() {
+    // create an array of 2D matrices to return if indexed
+    ptrs_ = new device_ptr<T>[row_w_];
+    for (int i=0; i < row_w_; i++) {
+        // each matrix should reference piece of memory
+        ptrs_[i] = device_ptr<T>();
+    }
+}
+
+template <class T>
+void Matrix1D<T>::write(T* buf) {
+    assert(buf != NULL);
+
+    cudaError_t rval = cudaMemcpy(data_, buf, size_bytes, cudaMemcpyHostToDevice);
+    assert(rval == cudaSuccess);
+}
+
+template <class T>
+T* Matrix1D<T>::read(T* buf) {
+    if (buf == NULL) {
+        buf = new T[row_w_];
+    }
+
+    cudaError_t rval = cudaMemcpy(buf, data_, size_bytes, cudaMemcpyDeviceToHost);
+    assert(rval == cudaSuccess);
+    return buf;
+}
+
+
+template <class T>
+device_ptr<T>::device_ptr(T* data_loc) {
+    size_bytes = sizeof(T);
+
+    // Allocate or reference data. Record if new allocated for later deletion.
+    if (data_loc == NULL) {
+        internal_ = false;
+        cudaError_t rval = cudaMalloc(&data_, size_bytes);
+        assert(rval == cudaSuccess);
     }
     else {
         internal_ = true;
         data_ = data_loc;
     }
 }
+
 template <class T>
-Vector1D<T>::~Vector1D() {
+device_ptr<T>::~device_ptr() {
     // only delete if originally new allocated
-    if (!internal_) delete[] data_;
+    if (!internal_) cudaFree(data_);
+}
+
+template <class T>
+void device_ptr<T>::write(T* buf) {
+    assert(buf != NULL);
+
+    cudaError_t rval = cudaMemcpy(data_, buf, size_bytes, cudaMemcpyHostToDevice);
+    assert(rval == cudaSuccess);
+}
+
+template <class T>
+T* device_ptr<T>::read(T* buf) {
+    if (buf == NULL) {
+        buf = new T;
+    }
+
+    cudaError_t rval = cudaMemcpy(buf, data_, size_bytes, cudaMemcpyDeviceToHost);
+    assert(rval == cudaSuccess);
+    return buf;
 }
--- dim_cuda.h
+++ dim_cuda.h
@@ -7,7 +7,8 @@ namespace gpu {
 
 // prototype data sub-structures
 template <class T> class Matrix2D;
-template <class T> class Vector1D;
+template <class T> class Matrix1D;
+template <class T> class device_ptr;
 
 /* A 3D matrix, which is essentially a list of 2D matrixes.
  * Data is stored contiguously in device memory, and indexing returns a 
@@ -17,12 +18,13 @@ template <class T>
 class Matrix3D {
     public:
         /* Create a 3D matrix of the specified size.
-         * Data will be new allocated and deleted upon deconstruction.
+         * Data will be new allocated in device memory and deleted upon deconstruction.
          * \param[in] num_layers Number of 2D matrix layers
          * \param[in] column_h Height of a child 2D matrix's column
          * \param[in] row_w Width of a child 2D matrix's row
+         * \param[in] preprocess Create indexable objects at contruction, otherwise at first index. Default: true
          */
-        Matrix3D(size_t num_layers, size_t column_h, size_t row_w);
+        Matrix3D(size_t num_layers, size_t column_h, size_t row_w, bool preprocess=true);
         /* Data is deleted upon deconstruction, and any child matrices
          * will index to invalid data. */
         ~Matrix3D();
@@ -31,8 +33,19 @@ class Matrix3D {
          */
         Matrix2D<T>& operator [](size_t i) const {
             assert(i < num_layers_);
+            if (list_ == NULL) process();
             return list_[i];
         };
+
+        /* read data from the device.
+         * If no buffer given, one will be new allocated and must be freed by caller.
+         * \param[in] buf Buffer to write memory into. */
+        void write(T* buf);
+        /* Write the contents of the buffer into device memory.
+         * \param[in] buf Buffer to copy from.
+         */
+        T* read(T* buf=NULL);
+
     
         /* \return Number of 2D sub-matrices */
         size_t num() {return num_layers_; };
@@ -46,10 +59,14 @@ class Matrix3D {
         size_t num_layers_;
         size_t column_h_;
         size_t row_w_;
+        // size of the array in bytes
+        size_t size_bytes;
         // pointer to memory chunk containing data
         T* data_;
         // an array to hold sub-matrices
         Matrix2D<T>* list_;
+        // create sub-data array
+        void process();
 };
 
 /* A 2D matrix, which is essentially a list of 1D matrixes.
@@ -65,18 +82,29 @@ class Matrix2D {
          * \param[in] column_h Height of a child 2D matrix's column
          * \param[in] row_w Width of a child 2D matrix's row
          * \param[in] data_loc Pointer to the data to reference. Default NULL means new allocation.
+         * \param[in] preprocess Create indexable objects at contruction, otherwise at first index. Default: true         
          */
-        Matrix2D(size_t column_h, size_t row_w, T* data_loc=NULL);
+        Matrix2D(size_t column_h, size_t row_w, T* data_loc=NULL, bool preprocess=true);
         /* Deconstuction only deletes data if it was created in the constructor.
          * If that happens, then child 1D matrixes will index to invalid memory.*/
         ~Matrix2D();
         
         /* Return a 1D matrix which references a piece of the contiguous memory.
          */
-        Vector1D<T>& operator [](size_t i) const {
+        Matrix1D<T>& operator [](size_t i) const {
             assert(i < column_h_);
+            if (layer_ == NULL) process();
             return layer_[i];
         };
+
+        /* read data from the device.
+         * If no buffer given, one will be new allocated and must be freed by caller.
+         * \param[in] buf Buffer to write memory into. */
+        void write(T* buf);
+        /* Write the contents of the buffer into device memory.
+         * \param[in] buf Buffer to copy from.
+         */
+        T* read(T* buf=NULL);
 
         /* \return Height of column */
         size_t height() {return column_h_; };
@@ -87,36 +115,51 @@ class Matrix2D {
         // dimensions
         size_t column_h_;
         size_t row_w_;
+        // size of the array in bytes
+        size_t size_bytes;
         // pointer to memory chunk containing data
         T* data_;
         // whether this is a sub-matrix, if not then data must be deleted
         bool internal_;
         // an array to hold sub-matrices
-        Vector1D<T>* layer_;
+        Matrix1D<T>* layer_;
+        // create sub-data array
+        void process();
 };
 
-/* A 1D matrix, whose data is stored contigously in memory.
+/* A 1D matrix (transpose of vector), whose data is stored contigously in memory.
  * Indexing returns a reference to an entry in that memory.
  */
 template <class T>
-class Vector1D {
+class Matrix1D {
     public:
         /* Create a 1D matrix of the specified size.
          * If data_loc is not NULL, then the data will be set to 
          * that location and will not be deleted upon deconstuction.
          * \param[in] row_w Width of a child 2D matrix's row
          * \param[in] data_loc Pointer to the data to reference. Default NULL means new allocation.
+         * \param[in] preprocess Create indexable objects at contruction, otherwise at first index. Default: true
          */
-        Vector1D(size_t row_w, T* data_loc=NULL);
+        Matrix1D(size_t row_w, T* data_loc=NULL, bool preprocess=true);
         /* Deconstuction only deletes data if it was created in the constructor */
-        ~Vector1D();
+        ~Matrix1D();
 
         /* Return a reference to a piece of the contiguous memory.
          */
         T& operator [](size_t i) const {
             assert(i < row_w_);
-            return data_[i];
+            if (ptrs_ == NULL) process();
+            return ptrs_[i];
         };
+
+        /* read data from the device.
+         * If no buffer given, one will be new allocated and must be freed by caller.
+         * \param[in] buf Buffer to write memory into. */
+        void write(T* buf);
+        /* Write the contents of the buffer into device memory.
+         * \param[in] buf Buffer to copy from.
+         */
+        T* read(T* buf=NULL);
 
         /* \return Width of row */
         size_t width() {return row_w_; };
@@ -124,9 +167,41 @@ class Vector1D {
     private:
         // dimensions
         size_t row_w_;
+        // size of the array in bytes
+        size_t size_bytes;
         // pointer to memory chunk containing data
         T* data_;
         // whether this is a sub-matrix, if not then data must be deleted
+        bool internal_;
+        // an array to hold the device pointers to memory
+        device_ptr* ptrs_;
+        // create sub-data array
+        void process();
+};
+
+/* A pointer to an object in device memory.
+ */
+template <class T>
+class device_ptr {
+    public:
+        device_ptr(T* data_loc=NULL);
+        ~device_ptr();
+
+        /* read data from the device.
+         * If no buffer given, one will be new allocated and must be freed by caller.
+         * \param[in] buf Buffer to write memory into. */
+        void write(T* buf);
+        /* Write the contents of the buffer into device memory.
+         * \param[in] buf Buffer to copy from.
+         */
+        T* read(T* buf=NULL);
+
+    private:
+        // size of the object in bytes
+        size_t size_bytes;
+        // pointer to the object in device memory
+        T* data_;
+        // whether this is a sub-object, if not then data must be deleted
         bool internal_;
 };
 
