81c77e64fe1da7d610c6a8eea13697eedd653377
Adam Klein <adam3207@gmail.com>
2022-08-01 01:15:40 -0700

    Added zero value for summation in matMulti

--- gpu_math.cuh
+++ gpu_math.cuh
@@ -7,7 +7,6 @@ int MULT_BLOCK_SIZE = 8;
 int SUM_BLOCK_SIZE = 32;
 int SUM_DIVIDER = 8;
 
-
 namespace DIM_CUDA_HIDDEN {
 
 /* Take matrix A and vector x and store x[i]*A[j][i] to B[j][i].
@@ -33,16 +32,16 @@ __global__ void gpu_math_multiply_to_cache_matrix(T* A, U* x, V* B, int width, i
 /* Take matrix B and "squash" its rows into skinnier rows using summation.
  * constant SUM_DIVIDER determines the number of B columns that are summed into each C column.
  * NOTE: the width of C should be ceiling(width/SUM_DIVIDER)
- * NOTE: assumes the values of C have been set to zero
  * \param[in] B pointer to input matrix
  * \param[in] C pointer to cache matrix to store sums
  * \param[in] height number of rows in B and C
  * \param[in] width number of columns B
  * \param[in] width_C number of columns in C
  * \param[in] sum_divider the number of B columns to sum into each C column
+ * \param[in] zero Value to set an object to before summing
  */
 template <class T>
-__global__ void gpu_math_first_summation(T* B, T* C, int width, int height, int width_C, int sum_divider) {
+__global__ void gpu_math_first_summation(T* B, T* C, int width, int height, int width_C, int sum_divider, T zero) {
     // get location
     int i = (blockIdx.x * blockDim.x) + threadIdx.x;
     int j = (blockIdx.y * blockDim.y) + threadIdx.y;
@@ -54,20 +53,21 @@ __global__ void gpu_math_first_summation(T* B, T* C, int width, int height, int 
     int ind_C = i + (j * width_C);
 
     // sum the columns of B into C
+    C[ind_C] = zero;
     for (int offset=0; offset < sum_divider && x + offset < width; offset++) {
         C[ind_C] += B[ind_B + offset];
     }
 }
 
 /* Sum the rows of matrix C into column vector y.
- * NOTE: assumes the values of y have been set to zero
  * \param[in] C pointer to input matrix
  * \param[in] y pointer to output column vector
  * \param[in] height number of rows in C and elements in y
  * \param[in] width number of columns C
+ * \param[in] zero Value to set an object to before summing
  */
 template <class T>
-__global__ void gpu_math_final_summation(T* C, T* y, int width, int height) {
+__global__ void gpu_math_final_summation(T* C, T* y, int width, int height, T zero) {
     // get location
     int i = (blockIdx.x * blockDim.x) + threadIdx.x;
     int j = (blockIdx.y * blockDim.y) + threadIdx.y;
@@ -77,6 +77,7 @@ __global__ void gpu_math_final_summation(T* C, T* y, int width, int height) {
     int ind = i + (j * width);
 
     // sum of the columns of C into y
+    y[j] = zero;
     for (int offset=0; offset < width; offset++) {
         y[j] += C[ind + offset];
     }
@@ -93,9 +94,10 @@ namespace gpu {
  * \param[in] y output matrix of type V
  * \param[in] height number of rows in A and B
  * \param[in] width number of columns in A and B, also number of elements in x
+ * \param[in] zero value to set V object to before summing
  */
 template <class T, class U, class V>
-void matMulti(Matrix2D<T> &A, Vector1D<U> &x, Vector1D<V> &y) {
+void matMulti(Matrix2D<T> &A, Vector1D<U> &x, Vector1D<V> &y, V zero) {
     // check that sizes match up
     assert(A.width() == x.size());
     assert(A.height() == y.size());
@@ -134,7 +136,7 @@ void matMulti(Matrix2D<T> &A, Vector1D<U> &x, Vector1D<V> &y) {
 
     // First summation stage: Collapse the rows of B into a skinnier matrix C
     DIM_CUDA_HIDDEN::gpu_math_first_summation<V><<<numBlocks, blockSize>>>(
-        B, C, static_cast<int>(A.width()), static_cast<int>(A.height()), width_C, SUM_DIVIDER
+        B, C, static_cast<int>(A.width()), static_cast<int>(A.height()), width_C, SUM_DIVIDER, zero
     );
 
     // set block params for final summation
@@ -145,7 +147,7 @@ void matMulti(Matrix2D<T> &A, Vector1D<U> &x, Vector1D<V> &y) {
 
     // Final Summation: Collapse the rows of C into the entries of y
     DIM_CUDA_HIDDEN::gpu_math_final_summation<V><<<numBlocks, blockSize>>>(
-        C, y.get_data(), width_C, static_cast<int>(y.size())
+        C, y.get_data(), width_C, static_cast<int>(y.size()), zero
     );
 
 
