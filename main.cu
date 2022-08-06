b55a7610957f5c4decfb9d28e972db42c281b853
Adam Klein <adam3207@gmail.com>
2022-08-06 14:14:26 -0700

    better test

--- main.cu
+++ main.cu
@@ -41,9 +41,7 @@ int main() {
     Timer timer(TIME_UNIT::us);
 
     // calculate Ax=y
-    for (int k=0; k < 32; k++) {
-        gpu::matMulti_opt<float, float, float>(A, x, y);
-    }
+    gpu::matMulti_opt<float, float, float>(A, x, y);
 
     timer.print("CUDA:");
 
@@ -53,38 +51,27 @@ int main() {
 
     initial_timer.print("CUDA (with transfers):");
 
-    /*
-    // print the values of y_out buffer
-    for (size_t i=0; i<test_size; i++) {
-        std::cout << y_out[i] << std::endl;
-    }
-    std::cout << std::endl;
-    delete[] y_out;
-    */
-
 
     timer.restart();
     // calculate the multiplication using the cpu
-    for (int k=0; k < 128; k++) {
-        for (int i=0; i<test_size; i++) {
-            for (int j=0; j<test_size; j++) {
-                y_setter[j] += A_setter[i+(j*test_size)] * x_setter[i];
-            }
+    for (int i=0; i<test_size; i++) {
+        for (int j=0; j<test_size; j++) {
+            y_setter[j] += A_setter[i+(j*test_size)] * x_setter[i];
         }
     }
 
     timer.print("CPU:");
 
-    /*
-    for (size_t i=0; i<test_size; i++) {
-        std::cout << y_setter[i] << std::endl;
-    }
-    */
+
+   //for (int i=0; i<test_size; i++) {
+   // std::cout << y_setter[i] << " -> " << y_out[i] << std::endl;
+   //}
 
 
     delete[] x_setter;
     delete[] A_setter;
     delete[] y_setter;
+    delete[] y_out;
 
     return 0;
 };
